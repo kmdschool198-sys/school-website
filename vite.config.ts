c8eb8a3ae53932318cd6ae28ab21e4de42582792
KruJames <kmdschool198@gmail.com>
2026-04-23 00:02:52 +0700

    chore: force redeploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// Triggering new deployment to ensure latest code is used.
 export default defineConfig({
   plugins: [react()],
 })
